734dc6518cfb6d40bd5e55b22b44ff12650644e0
Bela <bela.schinke@gmail.com>
2022-08-24 20:05:58 +0200

    Fixed dependencies

---
+++ PPClean/src/PPClean/DuplicateDetection/NaiveDetection.java
@@ -0,0 +1,44 @@
+package PPClean.DuplicateDetection;
+
+import PPClean.Data.Duplicate;
+import PPClean.Data.Record;
+import PPClean.Data.Table;
+import PPClean.Similarity.RecordSimilarity;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Duplicate detection algorithm which compares every record with every other
+ */
+public class NaiveDetection implements DuplicateDetection {
+
+    double threshold;
+
+    /**
+     * @param threshold Threshold in range [0,1], Records at least this similar are considered Duplicates
+     */
+    public NaiveDetection(double threshold) {
+        this.threshold = threshold;
+    }
+
+
+    /**
+     * @param table Table to check for duplicates
+     * @param recSim Similarity measure to use for comparing two records
+     * @return Set of detected duplicates
+     */
+    public Set<Duplicate> detect(Table table, RecordSimilarity recSim) {
+        List<Record> records = table.getData();
+        Set<Duplicate> duplicates = new HashSet<>();
+        int numComparisons = 0;
+        // BEGIN SOLUTION
+
+
+
+        // END SOLUTION
+        System.out.printf("Naive Detection found %d duplicates after %d comparisons%n", duplicates.size(), numComparisons);
+        return duplicates;
+    }
+}
---
+++ PPClean/src/Test/Task5.java
@@ -0,0 +1,40 @@
+package Test;
+
+import org.junit.jupiter.api.Test;
+import PPClean.Data.Duplicate;
+import PPClean.Data.Table;
+import PPClean.Data.TableFactory;
+import PPClean.DuplicateDetection.SortedNeighborhoodDetection;
+import PPClean.Helper;
+import PPClean.Performance.Performance;
+import PPClean.Performance.Score;
+import PPClean.Similarity.*;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Task5 {
+
+    Table inputTable = TableFactory.getDefaultInputTable();
+    Set<Duplicate> groundTruth = Helper.readDuplicatesFromDefaultGT();
+    Performance performance = Performance.initInstance(groundTruth);
+
+    @Test
+    void testSNHDetection() {
+        System.out.println("[Task5: testSNHDetection]");
+        System.out.println("Creating SingleAttributeEquality for name comparison (attributeIndex: 1)...");
+        SingleAttributeEquality nameAttributeEquality = new SingleAttributeEquality(1);
+        int[] keyComponents = {0, 3, 3, 3, 3, 3};
+        System.out.println("Creating SortedNeighborhoodDetection with the following parameteres:");
+        System.out.println("  similarity threshold: 0.8, window size: 4, key components: [0,3,3,3,3,3]");
+        SortedNeighborhoodDetection SNHDetection = new SortedNeighborhoodDetection(0.8, 4, keyComponents);
+        System.out.println("Applying SortedNeighborhoodDetection with SingleAttributeEquality...");
+        Set<Duplicate> duplicates = SNHDetection.detect(inputTable, nameAttributeEquality);
+        Score s = performance.evaluate(duplicates);
+        System.out.println("Expected results: Precision: 0.942, Recall: 0.732, F1-Score: 0.824");
+        assertEquals(s.getPrecision(), .942, 0.05);
+        assertEquals(s.getRecall(), .732, 0.05);
+        assertEquals(s.getF1(), .824, 0.05);
+    }
+}
